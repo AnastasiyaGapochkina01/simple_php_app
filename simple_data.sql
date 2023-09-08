ef4b3312e89ad34f528c30e45aa2c78769fe2a66
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2023-09-08 13:57:02 +0300

    Update simple_data.sql

--- simple_data.sql
+++ simple_data.sql
@@ -9,4 +9,4 @@ insert into cars values (1, 'Mercedes', 'A123BCD', 'black');
 insert into cars values (2, 'BMW', 'B456FEG', 'red');
 insert into cars values (3, 'Honda', 'C789HIJ', 'gray');
 insert into cars values (4, 'Toyota', 'D012KLM', 'black');
-insert inti cars values (5, 'Ford', 'F345NOP', 'white');
+insert into cars values (5, 'Ford', 'F345NOP', 'white');
